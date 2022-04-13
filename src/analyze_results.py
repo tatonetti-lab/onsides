fbd28b785efb0d605f9f037afc9c25cb40139730
Nicholas Tatonetti <nick.tatonetti@gmail.com>
2022-04-13 09:20:15 -0400

    added argparse and refset

--- src/analyze_results.py
+++ src/analyze_results.py
@@ -2,54 +2,66 @@ import os
 import sys
 import torch
 import random
+import argparse
 import pandas as pd
 import numpy as np
 
 sys.path.append(os.path.abspath("./src"))
 import fit_clinicalbert as cb
 
-model = cb.ClinicalBertClassifier()
+if __name__ == '__main__':
 
-model_file = sys.argv[1]
-print(f"Loading model from {model_file}")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str, required=True)
 
-np_random_seed = int(model_file.split('_')[1])
-random_state = int(model_file.split('_')[2])
-EPOCHS = int(model_file.split('_')[3])
-LR = model_file.split('_')[4].split('.')[0]
-prefix = model_file.split('_')[0].split('/')[-1]
+    args = parser.parse_args()
 
-print(f" prefix: {prefix}")
-print(f" np_random_seed: {np_random_seed}")
-print(f" random_state: {random_state}")
-print(f" EPOCHS: {EPOCHS}")
-print(f" LR: {LR}")
+    model_file = args.model
+    print(f"Loading model from {model_file}")
 
-model.load_state_dict(torch.load(model_file))
+    if len(model_file.split('_')) != 6:
+        raise Exception("Model filename not in format expected: {prefix}_{refset}_{np_random_seed}_{random_state}_{EPOCHS}_{LR}.pth")
 
-# loading and re-splitting the data
-datapath = './data/clinical_bert_reference_set.txt'
-df = pd.read_csv(datapath)
+    refset = int(model_file.split('_')[1])
+    np_random_seed = int(model_file.split('_')[2])
+    random_state = int(model_file.split('_')[3])
+    EPOCHS = int(model_file.split('_')[4])
+    LR = model_file.split('_')[5].split('.')[0]
 
-# randomly select by drug/label
-druglist = sorted(set(df['drug']))
+    prefix = model_file.split('_')[0].split('/')[-1]
 
-random.seed(np_random_seed)
-random.shuffle(druglist)
+    print(f" prefix: {prefix}")
+    print(f" np_random_seed: {np_random_seed}")
+    print(f" random_state: {random_state}")
+    print(f" EPOCHS: {EPOCHS}")
+    print(f" LR: {LR}")
 
-drugs_train, drugs_val, drugs_test = np.split(druglist, [int(0.8*len(druglist)), int(0.9*len(druglist))])
+    model = cb.ClinicalBertClassifier()
+    model.load_state_dict(torch.load(model_file))
 
-print(f"Split labels in train, val, test by drug:")
-print(len(drugs_train), len(drugs_val), len(drugs_test))
+    # loading and re-splitting the data
+    datapath = './data/clinical_bert_reference_set.txt'
+    df = pd.read_csv(datapath)
 
-df_train = df[df['drug'].isin(drugs_train)]
-df_val = df[df['drug'].isin(drugs_val)]
-df_test = df[df['drug'].isin(drugs_test)]
+    # randomly select by drug/label
+    druglist = sorted(set(df['drug']))
 
-print(len(df_train), len(df_val), len(df_test))
+    random.seed(np_random_seed)
+    random.shuffle(druglist)
 
-outputs = cb.evaluate(model, df_test)
-npoutputs = [x.cpu().detach().numpy() for x in outputs]
-predictions = np.vstack(npoutputs)
+    drugs_train, drugs_val, drugs_test = np.split(druglist, [int(0.8*len(druglist)), int(0.9*len(druglist))])
 
-np.savetxt(f'./results/{prefix}_{np_random_seed}_{random_state}_{EPOCHS}_{LR}.csv', predictions, delimiter=',')
+    print(f"Split labels in train, val, test by drug:")
+    print(len(drugs_train), len(drugs_val), len(drugs_test))
+
+    df_train = df[df['drug'].isin(drugs_train)]
+    df_val = df[df['drug'].isin(drugs_val)]
+    df_test = df[df['drug'].isin(drugs_test)]
+
+    print(len(df_train), len(df_val), len(df_test))
+
+    outputs = cb.evaluate(model, df_test)
+    npoutputs = [x.cpu().detach().numpy() for x in outputs]
+    predictions = np.vstack(npoutputs)
+
+    np.savetxt(f'./results/{prefix}_{np_random_seed}_{random_state}_{EPOCHS}_{LR}.csv', predictions, delimiter=',')
