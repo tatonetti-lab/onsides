b8d05db164f59c707f78eed85490f558c257464b
Nicholas Tatonetti <nick.tatonetti@gmail.com>
2022-04-14 21:43:19 -0400

    updated how string is parsed

--- src/analyze_results.py
+++ src/analyze_results.py
@@ -43,7 +43,7 @@ if __name__ == '__main__':
     model.load_state_dict(torch.load(model_filepath))
 
     # loading and re-splitting the data
-    datapath = f'../data/ref{refset}_clinical_bert_reference_set.txt'
+    datapath = f'./data/ref{refset}_clinical_bert_reference_set.txt'
     df = pd.read_csv(datapath)
 
     # randomly select by drug/label
