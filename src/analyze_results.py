62b074e9d7e9175c5bda316cb9d9b2e42ba6c7c8
Nicholas Tatonetti <nick.tatonetti@gmail.com>
2022-04-18 10:09:01 -0400

    updated analyze results to work with new file naming scheme

--- src/analyze_results.py
+++ src/analyze_results.py
@@ -17,17 +17,25 @@ if __name__ == '__main__':
     model_file = os.path.split(model_filepath)[-1]
 
     print(f"Loading model from {model_file}")
+    fnnoext = os.path.split(model_file)[-1].split('.')[0]
 
-    if len(model_file.split('_')) != 6:
-        raise Exception("Model filename not in format expected: {prefix}_{refset}_{np_random_seed}_{random_state}_{EPOCHS}_{LR}.pth")
+    if not len(fnnoext.split('_')) in (6, 8):
+        raise Exception("Model filename not in format expected: {prefix}_{refset}_{np_random_seed}_{random_state}_{EPOCHS}_{LR}.pth or {prefix}_{refset}_{np_random_seed}_{random_state}_{EPOCHS}_{LR}_{MAX_LENGTH}_{BATCH_SIZE}.pth")
 
-    refset = int(model_file.split('_')[1])
-    np_random_seed = int(model_file.split('_')[2])
-    random_state = int(model_file.split('_')[3])
-    EPOCHS = int(model_file.split('_')[4])
-    LR = model_file.split('_')[5].split('.')[0]
+    refset = int(fnnoext.split('_')[1])
+    np_random_seed = int(fnnoext.split('_')[2])
+    random_state = int(fnnoext.split('_')[3])
+    EPOCHS = int(fnnoext.split('_')[4])
+    LR = fnnoext.split('_')[5]
 
-    prefix = model_file.split('_')[0]
+    if len(fnnoext.split('_')) == 8:
+        max_length = fnnoext.split('_')[6]
+        batch_size = fnnoext.split('_')[7]
+    else:
+        max_length = 128
+        batch_size = 128
+
+    prefix = fnnoext.split('_')[0]
 
     print(f" prefix: {prefix}")
     print(f" refset: {refset}")
@@ -35,6 +43,8 @@ if __name__ == '__main__':
     print(f" random_state: {random_state}")
     print(f" EPOCHS: {EPOCHS}")
     print(f" LR: {LR}")
+    print(f" max_length: {max_length}")
+    print(f" batch_size: {batch_size}")
 
     sys.path.append(os.path.abspath("./src"))
     import fit_clinicalbert as cb
@@ -68,18 +78,20 @@ if __name__ == '__main__':
     npoutputs = [x.cpu().detach().numpy() for x in outputs]
     predictions = np.vstack(npoutputs)
 
-    np.savetxt(f'./results/{prefix}-test_{refset}_{np_random_seed}_{random_state}_{EPOCHS}_{LR}.csv', predictions, delimiter=',')
+    file_parameters = f'{refset}_{np_random_seed}_{random_state}_{EPOCHS}_{LR}_{max_length}_{batch_size}'
+
+    np.savetxt(f'./results/{prefix}-test_{file_parameters}.csv', predictions, delimiter=',')
 
     print(f"Evaluating validation data...")
     outputs = cb.evaluate(model, df_val)
     npoutputs = [x.cpu().detach().numpy() for x in outputs]
     predictions = np.vstack(npoutputs)
 
-    np.savetxt(f'./results/{prefix}-valid_{refset}_{np_random_seed}_{random_state}_{EPOCHS}_{LR}.csv', predictions, delimiter=',')
+    np.savetxt(f'./results/{prefix}-valid_{file_parameters}.csv', predictions, delimiter=',')
 
     print(f"Evaluating training data...")
     outputs = cb.evaluate(model, df_train)
     npoutputs = [x.cpu().detach().numpy() for x in outputs]
     predictions = np.vstack(npoutputs)
 
-    np.savetxt(f'./results/{prefix}-train_{refset}_{np_random_seed}_{random_state}_{EPOCHS}_{LR}.csv', predictions, delimiter=',')
+    np.savetxt(f'./results/{prefix}-train_{file_parameters}.csv', predictions, delimiter=',')
