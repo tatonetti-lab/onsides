1b8bcd66f52648c803e5bd05da951a890990ad0d
ntatonetti <npt2105@hoor.local>
2022-04-19 06:57:20 -0400

    refactored example construction scripts to make them more mdular; added a notebook to explore the relationship between number of tokens and number of words on the labels

--- src/construct_application_data.py
+++ src/construct_application_data.py
@@ -17,61 +17,35 @@ import csv
 import tqdm
 import argparse
 
+from construct_training_data import get_args
+from construct_training_data import load_meddra
+from construct_training_data import generate_examples
+
 def main():
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--method', help='Choose which example generation method to use. See code for details.', type=int, required=True)
-    parser.add_argument('--dir', help='Path to the directory that contains the parsed SPL files. The program expects that there will be a file named *_adverse_reactions.txt for each label.', type=str, required=True)
-    parser.add_argument('--nwords', help='The number of words to grab from either side of the event mention to generate the training example', type=int, default=64)
+    addl_args = [
+        {
+            'args': ['--dir'],
+            'kwargs': {
+                'help': 'Path to the directory that contains the parsed SPL files.',
+                'type': str,
+                'required': True
+            }
+        }
+    ]
+    args, sub_event, sub_nonsense, prepend_event, suffix, section_display_name = get_args(addl_args)
 
-    args = parser.parse_args()
-
-    sub_event = False
-    sub_nonsense = False
-    prepend_event = False
-
-    if args.method == 0:
-        sub_event = True
-        prepend_event = True
-    elif args.method == 1:
-        sub_event = False
-        prepend_event = True
-    elif args.method == 2:
-        sub_event = True
-        prepend_event = False
-    elif args.method == 3:
-        sub_event = False
-        prepend_event = False
-    elif args.method == 4:
-        sub_event = True
-        sub_nonsense = True
-        prepend_event = False
-    else:
-        raise Exception(f"Expected method argument to be an integer value (0, 1, 2, 3, or 4). Got {args.method}")
-
-    # load preferred terms and lower level terms
-    meddra_fn = './data/meddra_llt_pt_map.txt'
-    meddra_fh = open(meddra_fn)
-    reader = csv.reader(meddra_fh, delimiter='|')
-    header = next(reader)
-
-    llts = dict()
-
-    for row in reader:
-        data = dict(zip(header, row))
-        llts[data['llt_concept_id']] = data['llt_concept_name'].lower()
-
-    meddra_fh.close()
+    llts = load_meddra()
 
     # derive a drug list from the training and testing data provided
-    all_drugs = set([fn.split('_')[0] for fn in os.listdir(args.dir) if fn.endswith('adverse_reactions.txt')])
+    all_drugs = set([fn.split('_')[0] for fn in os.listdir(args.dir) if fn.endswith(suffix)])
     file_prefix = os.path.split(args.dir)[-1]
 
     print(f"Found {len(all_drugs)} total drugs")
 
-    outfn = f'./data/{file_prefix}_app{args.method}_nwords{args.nwords}_clinical_bert_application_set.txt'
+    outfn = f'./data/{file_prefix}_app{args.method}_nwords{args.nwords}_clinical_bert_application_set_{args.section}.txt'
     print(f" Application data will be written to {outfn}")
-    
+
     outfh = open(outfn, 'w')
     writer = csv.writer(outfh)
     writer.writerow(['drug', 'llt_id', 'llt', 'string'])
@@ -80,7 +54,7 @@ def main():
         #print(f"Generating application data for: {drug}")
 
         # load text from adverse events section
-        ar_file_path = os.path.join(args.dir, f'{drug}_adverse_reactions.txt')
+        ar_file_path = os.path.join(args.dir, f'{drug}_{suffix}')
         if not os.path.exists(ar_file_path):
             raise Exception(f"Did not file an adverse event file for {ar_file_path}")
 
@@ -99,38 +73,10 @@ def main():
                 llts_mentioned.add(llt_id)
                 string_mentioned.add(llt)
 
-                #print(llt)
-                #print(len(ar_text.split(llt)))
+                example_strings = generate_examples(ar_text, llt, args.nwords, sub_event, sub_nonsense, prepend_event)
 
-                parts = ar_text.split(llt)
-
-                size_of_llt = len(llt.split())
-                size_of_parts = args.nwords - size_of_llt
-
-                if len(parts) == 1:
-                    raise Exception("Parts has length of 1 which shouldn't be possible.")
-
-                for i in range(len(parts)-1):
-                    # print(parts[i].split()[-1*size_of_parts:])
-                    EVENT_STRING = llt
-                    if sub_event:
-                        EVENT_STRING = 'EVENT'
-                    if sub_nonsense:
-                        EVENT_STRING = 'YIHFKEHDK'
-
-                    START_STRING = ''
-                    if prepend_event:
-                        START_STRING = llt
-
-                    example_string = START_STRING + ' ' + ' '.join(parts[i].split()[-1*size_of_parts:] + [EVENT_STRING] + parts[i+1].split()[:size_of_parts])
-
+                for example_string in example_strings:
                     writer.writerow([drug, llt_id, llt, example_string])
-
-                # break
-
-        #print(f"\tNumber of terms mentioned in text: {len(llts_mentioned)}")
-
-    #print(f"Finished generating application set.")
 
     outfh.close()
 
--- src/construct_training_data.py
+++ src/construct_training_data.py
@@ -15,12 +15,16 @@ import sys
 import csv
 import argparse
 
-def main():
+def get_args(addl_args = None):
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--method', type=int, required=True)
     parser.add_argument('--nwords', help='The number of words to grab from either side of the event mention to generate the training example', type=int, default=64)
     parser.add_argument('--section', help='Designate which section of the label to parse. Options are AR (adverse reactions), BW (boxed warnings), or WP (warnings and precautions)', type=str, default='AR')
+
+    if not addl_args is None and type(addl_args) is list:
+        for addl_arg in addl_args:
+            parser.add_argument(*addl_arg['args'], **addl_arg['kwargs'])
 
     args = parser.parse_args()
 
@@ -47,20 +51,6 @@ def main():
     else:
         raise Exception(f"Expected method argument to be an integer value (0, 1, 2, 3, or 4). Got {args.method}")
 
-    # load preferred terms and lower level terms
-    meddra_fn = './data/meddra_llt_pt_map.txt'
-    meddra_fh = open(meddra_fn)
-    reader = csv.reader(meddra_fh, delimiter='|')
-    header = next(reader)
-
-    llts = dict()
-
-    for row in reader:
-        data = dict(zip(header, row))
-        llts[data['llt_concept_id']] = data['llt_concept_name'].lower()
-
-    meddra_fh.close()
-
     suffix = None
     section_display_name = None
     if args.section == 'AR':
@@ -77,6 +67,91 @@ def main():
         section_display_name = 'WARNINGS AND PRECAUTIONS'
     else:
         raise Exception(f"ERROR: Unknown section specificed: {args.section}")
+
+    return args, sub_event, sub_nonsense, prepend_event, suffix, section_display_name
+
+def load_meddra():
+    # load preferred terms and lower level terms
+    meddra_fn = './data/meddra_llt_pt_map.txt'
+    meddra_fh = open(meddra_fn)
+    reader = csv.reader(meddra_fh, delimiter='|')
+    header = next(reader)
+
+    llts = dict()
+
+    for row in reader:
+        data = dict(zip(header, row))
+        llts[data['llt_concept_code']] = data['llt_concept_name'].lower()
+
+    meddra_fh.close()
+
+    return llts
+
+def get_annotations(drug, section_display_name):
+
+    final_ref_fn = './data/200_manual_annotations_csv/FinalReferenceStandard200Labels.csv'
+    final_ref_fh = open(final_ref_fn)
+    reader = csv.reader(final_ref_fh, delimiter='|')
+    header = next(reader)
+
+    pts_annotated = set()
+    llts_annotated = set()
+    string_annotated = set()
+
+    for row in reader:
+        data = dict(zip(header, row))
+
+        if not data['Drug Name'] == drug:
+            continue
+
+        if not data['Section Display Name'] == section_display_name:
+            continue
+
+        pts_annotated.add(data['PT ID'])
+        llts_annotated.add(data['LLT ID'])
+        string_annotated.add(data['Matching String'].lower())
+
+    final_ref_fh.close()
+
+    return pts_annotated, llts_annotated, string_annotated
+
+def generate_examples(ar_text, llt, nwords, sub_event, sub_nonsense, prepend_event):
+    parts = ar_text.split(llt)
+
+    size_of_llt = len(llt.split())
+
+    # NOTE: BERT has a limit of 512 tokens, words that are not in BERTs
+    # NOTE: dictionary are split into subwords and tokenized. So the actual
+    # NOTE: number of tokens is more than the number of words. We initially
+    # NOTE: used ~128.
+    size_of_parts = nwords - size_of_llt
+
+    if len(parts) == 1:
+        raise Exception("Parts has length of 1 which shouldn't be possible.")
+
+    strings = list()
+    for i in range(len(parts)-1):
+        # print(parts[i].split()[-1*size_of_parts:])
+        EVENT_STRING = llt
+        if sub_event:
+            EVENT_STRING = 'EVENT'
+        if sub_nonsense:
+            EVENT_STRING = 'YIHFKEHDK'
+
+        START_STRING = ''
+        if prepend_event:
+            START_STRING = llt
+
+        example_string = START_STRING + ' ' + ' '.join(parts[i].split()[-1*size_of_parts:] + [EVENT_STRING] + parts[i+1].split()[:size_of_parts])
+        strings.append(example_string)
+
+    return strings
+
+def main():
+
+    args, sub_event, sub_nonsense, prepend_event, suffix, section_display_name = get_args()
+
+    llts = load_meddra()
 
     # derive a drug list from the training and testing data provided
     train_drugs = set([fn.split('_')[0] for fn in os.listdir('./data/200_training_set') if fn.find(suffix) != -1])
@@ -100,36 +175,13 @@ def main():
     for drug in all_drugs:
         print(f"Generating reference data for: {drug}")
 
-        # ADVERSE REACTIONS Section
-        final_ref_fn = './data/200_manual_annotations_csv/FinalReferenceStandard200Labels.csv'
-        final_ref_fh = open(final_ref_fn)
-        reader = csv.reader(final_ref_fh, delimiter='|')
-        header = next(reader)
-
-        pts_annotated = set()
-        llts_annotated = set()
-        string_annotated = set()
-
-        for row in reader:
-            data = dict(zip(header, row))
-
-            if not data['Drug Name'] == drug:
-                continue
-
-            if not data['Section Display Name'] == section_display_name:
-                continue
-
-            pts_annotated.add(data['PT ID'])
-            llts_annotated.add(data['LLT ID'])
-            string_annotated.add(data['Matching String'].lower())
-
-        final_ref_fh.close()
+        pts_annotated, llts_annotated, string_annotated = get_annotations(drug, section_display_name)
 
         print(f"\tPreferred terms annotated: {len(pts_annotated)}")
         print(f"\tLower level terms annotated: {len(llts_annotated)}")
         print(f"\tIntersection of terms with local meddra map: {len(string_annotated & set(llts.values()))}")
 
-        # load text from adverse events section
+        # load text from the desired (e.g. ADVERSE REACTIONS) section
         ar_file_path = f'./data/200_training_set/{drug}_{suffix}'
         if os.path.exists(ar_file_path):
             ar_fh = open(ar_file_path)
@@ -141,6 +193,7 @@ def main():
                 raise Exception("Couldn't file adverse reactions file in either the training or testing set.")
 
         ar_text = ' '.join(ar_fh.read().split()).lower()
+
         print(f"\tNumber of words in {section_display_name} text: {len(ar_text.split())}")
 
         # find all the llts that are mentioned in the text
@@ -156,44 +209,29 @@ def main():
                 llts_mentioned.add(llt_id)
                 string_mentioned.add(llt)
 
-                #print(llt)
-                #print(len(ar_text.split(llt)))
+                example_strings = generate_examples(ar_text, llt, args.nwords, sub_event, sub_nonsense, prepend_event)
 
-                parts = ar_text.split(llt)
+                # check if this event was annotated from the gold standard
+                if llt_id in llts_annotated:
+                    string_class = 'is_event'
+                    num_pos += len(example_strings)
+                else:
+                    string_class = 'not_event'
+                    num_neg += len(example_strings)
 
-                size_of_llt = len(llt.split())
-                # NOTE: BERT has a limit of 512 tokens, words that are not in BERTs
-                # NOTE: dictionary are split into subwords and tokenized. So the actual
-                # NOTE: number of tokens is more than the number of words. We initially
-                # NOTE: used ~128.
-                size_of_parts = args.nwords - size_of_llt
+                # NOTE: was using the matching string to match between the two,
+                # NOTE: but really should be using the LLT MedDRA identifier (above)
+                # NOTE: keeping this in here so we know what we did initially - NPT 4/19/22
 
-                if len(parts) == 1:
-                    raise Exception("Parts has length of 1 which shouldn't be possible.")
+                # if llt in string_annotated:
+                #     string_class = 'is_event'
+                #     num_pos += 1
+                # else:
+                #     string_class = 'not_event'
+                #     num_neg += 1
 
-                for i in range(len(parts)-1):
-                    # print(parts[i].split()[-1*size_of_parts:])
-                    EVENT_STRING = llt
-                    if sub_event:
-                        EVENT_STRING = 'EVENT'
-                    if sub_nonsense:
-                        EVENT_STRING = 'YIHFKEHDK'
-
-                    START_STRING = ''
-                    if prepend_event:
-                        START_STRING = llt
-
-                    example_string = START_STRING + ' ' + ' '.join(parts[i].split()[-1*size_of_parts:] + [EVENT_STRING] + parts[i+1].split()[:size_of_parts])
-                    if llt in string_annotated:
-                        string_class = 'is_event'
-                        num_pos += 1
-                    else:
-                        string_class = 'not_event'
-                        num_neg += 1
-
+                for example_string in example_strings:
                     writer.writerow([drug, llt_id, llt, string_class, example_string])
-
-                # break
 
 
         print(f"\tNumber of terms mentioned in text: {len(llts_mentioned)}")
@@ -215,10 +253,6 @@ def main():
     print(f" Total Neg: {total_num_neg}")
 
     outfh.close()
-
-
-
-
 
 if __name__ == '__main__':
     main()
