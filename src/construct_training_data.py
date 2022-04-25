234d311e16e3f6202a026733383e81abba1ae805
ntatonetti <npt2105@hoor.local>
2022-04-25 18:14:34 -0400

    added reference set option to group all sections together

--- src/construct_training_data.py
+++ src/construct_training_data.py
@@ -17,6 +17,17 @@ import csv
 import random
 import argparse
 
+section_suffices = {
+    'AR': 'adverse_reactions.txt',
+    'BW': 'boxed_warnings.txt',
+    'WP': 'warnings_and_precautions.txt'
+}
+section_display_names = {
+    'AR': 'ADVERSE REACTIONS',
+    'BW': 'BOXED WARNINGS',
+    'WP': 'WARNINGS AND PRECAUTIONS'
+}
+
 def get_args(addl_args = None):
 
     parser = argparse.ArgumentParser()
@@ -40,6 +51,7 @@ def get_args(addl_args = None):
     sub_event = False
     sub_nonsense = False
     prepend_event = False
+    prepend_section = False
     random_sampled_words = False
 
     if args.method == 0:
@@ -86,6 +98,10 @@ def get_args(addl_args = None):
         sub_event = True
         prepend_event = True
         args.prop_before = 0.875
+    elif args.method == 12:
+        sub_event = True
+        prepend_event = True
+        prepend_section = True
     else:
         raise Exception(f"Expected method argument to be an integer value (0, 1, 2, 3, 4, or 5). Got {args.method}")
 
@@ -93,24 +109,19 @@ def get_args(addl_args = None):
         raise Exception(f"ERROR: Unexpected value ({args.prop_before}) provided for --prop-before. Needs to be a float between 0 and 1.")
 
 
-    suffix = None
-    section_display_name = None
-    if args.section == 'AR':
+    sections = list()
+    if args.section in ('AR', 'BW', 'WP'):
         # ADVERSE REACTIONS
-        suffix = 'adverse_reactions.txt'
-        section_display_name = 'ADVERSE REACTIONS'
-    elif args.section == 'BW':
-        # BLACK BOX WARNINGS
-        suffix = 'boxed_warnings.txt'
-        section_display_name = 'BOXED WARNINGS'
-    elif args.section == 'WP':
-        # WARNINGS AND PRECAUTIONS
-        suffix = 'warnings_and_precautions.txt'
-        section_display_name = 'WARNINGS AND PRECAUTIONS'
+        sections.append(args.section)
+    elif args.section == 'ALL':
+        # all three sections
+        sections.append('AR')
+        sections.append('BW')
+        sections.append('WP')
     else:
         raise Exception(f"ERROR: Unknown section specificed: {args.section}")
 
-    return args, sub_event, sub_nonsense, prepend_event, suffix, section_display_name, random_sampled_words
+    return args, sub_event, sub_nonsense, prepend_event, sections, random_sampled_words
 
 def load_meddra():
     # load preferred terms and lower level terms
@@ -189,6 +200,7 @@ def generate_examples(ar_text, llt, nwords, sub_event, sub_nonsense, prepend_eve
     if sub_nonsense:
         EVENT_STRING = 'YIHFKEHDK'
 
+    # TODO: Implement prepend section option (ref12, currently not implemented)
     START_STRING = ''
     if prepend_event:
         START_STRING = llt
@@ -235,104 +247,105 @@ def main():
 
     random.seed(222)
 
-    args, sub_event, sub_nonsense, prepend_event, suffix, section_display_name, random_sampled_words = get_args()
+    args, sub_event, sub_nonsense, prepend_event, sections, random_sampled_words = get_args()
 
     llts = load_meddra()
-
-    # derive a drug list from the training and testing data provided
-    train_drugs = set([fn.split('_')[0] for fn in os.listdir('./data/200_training_set') if fn.find(suffix) != -1])
-    test_drugs = set([fn.split('_')[0] for fn in os.listdir('./data/200_test_set') if fn.find(suffix) != -1])
-
-    all_drugs = sorted(train_drugs | test_drugs)
-    print(f"Found {len(all_drugs)} total drugs with *{suffix} files.")
-
-    # generate reference data for a specific drug
-
-    # drug = 'ADCETRIS'
-
-    total_num_neg = 0
-    total_num_pos = 0
 
     outfn = f'./data/ref{args.method}_nwords{args.nwords}_clinical_bert_reference_set_{args.section}.txt'
     outfh = open(outfn, 'w')
     writer = csv.writer(outfh)
-    writer.writerow(['drug', 'llt_id', 'llt', 'class', 'string'])
+    writer.writerow(['section', 'drug', 'llt_id', 'llt', 'class', 'string'])
 
-    for drug in all_drugs:
-        print(f"Generating reference data for: {drug}")
+    total_num_neg = 0
+    total_num_pos = 0
 
-        pts_annotated, llts_annotated, string_annotated = get_annotations(drug, section_display_name)
+    for section in sections:
+        suffix = section_suffices[section]
+        section_display_name = section_display_names[section]
 
-        print(f"\tPreferred terms annotated: {len(pts_annotated)}")
-        print(f"\tLower level terms annotated: {len(llts_annotated)}")
-        print(f"\tIntersection of terms with local meddra map: {len(string_annotated & set(llts.values()))}")
+        # derive a drug list from the training and testing data provided
+        train_drugs = set([fn.split('_')[0] for fn in os.listdir('./data/200_training_set') if fn.find(suffix) != -1])
+        test_drugs = set([fn.split('_')[0] for fn in os.listdir('./data/200_test_set') if fn.find(suffix) != -1])
 
-        # load text from the desired (e.g. ADVERSE REACTIONS) section
-        ar_file_path = f'./data/200_training_set/{drug}_{suffix}'
-        if os.path.exists(ar_file_path):
-            ar_fh = open(ar_file_path)
-        else:
-            ar_file_path = f'./data/200_test_set/{drug}_{suffix}'
+        all_drugs = sorted(train_drugs | test_drugs)
+        print(f"Found {len(all_drugs)} total drugs with *{suffix} files.")
+
+        for drug in all_drugs:
+
+            print(f"Generating reference data for: {drug}")
+
+            pts_annotated, llts_annotated, string_annotated = get_annotations(drug, section_display_name)
+
+            print(f"\tPreferred terms annotated: {len(pts_annotated)}")
+            print(f"\tLower level terms annotated: {len(llts_annotated)}")
+            print(f"\tIntersection of terms with local meddra map: {len(string_annotated & set(llts.values()))}")
+
+            # load text from the desired (e.g. ADVERSE REACTIONS) section
+            ar_file_path = f'./data/200_training_set/{drug}_{suffix}'
             if os.path.exists(ar_file_path):
                 ar_fh = open(ar_file_path)
             else:
-                raise Exception("Couldn't file adverse reactions file in either the training or testing set.")
-
-        ar_text = ' '.join(ar_fh.read().split()).lower()
-
-        print(f"\tNumber of words in {section_display_name} text: {len(ar_text.split())}")
-
-        # find all the llts that are mentioned in the text
-
-        llts_mentioned = set()
-        string_mentioned = set()
-
-        num_pos = 0
-        num_neg = 0
-
-        for llt_id, llt in llts.items():
-            if ar_text.find(llt) != -1:
-                llts_mentioned.add(llt_id)
-                string_mentioned.add(llt)
-
-                example_strings = generate_examples(ar_text, llt, args.nwords, sub_event, sub_nonsense, prepend_event, random_sampled_words, args.prop_before)
-
-                # check if this event was annotated from the gold standard
-                if llt_id in llts_annotated:
-                    string_class = 'is_event'
-                    num_pos += len(example_strings)
+                ar_file_path = f'./data/200_test_set/{drug}_{suffix}'
+                if os.path.exists(ar_file_path):
+                    ar_fh = open(ar_file_path)
                 else:
-                    string_class = 'not_event'
-                    num_neg += len(example_strings)
+                    raise Exception("Couldn't file adverse reactions file in either the training or testing set.")
 
-                # NOTE: was using the matching string to match between the two,
-                # NOTE: but really should be using the LLT MedDRA identifier (above)
-                # NOTE: keeping this in here so we know what we did initially - NPT 4/19/22
+            ar_text = ' '.join(ar_fh.read().split()).lower()
 
-                # if llt in string_annotated:
-                #     string_class = 'is_event'
-                #     num_pos += 1
-                # else:
-                #     string_class = 'not_event'
-                #     num_neg += 1
+            print(f"\tNumber of words in {section_display_name} text: {len(ar_text.split())}")
 
-                for example_string in example_strings:
-                    writer.writerow([drug, llt_id, llt, string_class, example_string])
+            # find all the llts that are mentioned in the text
+
+            llts_mentioned = set()
+            string_mentioned = set()
+
+            num_pos = 0
+            num_neg = 0
+
+            for llt_id, llt in llts.items():
+                if ar_text.find(llt) != -1:
+                    llts_mentioned.add(llt_id)
+                    string_mentioned.add(llt)
+
+                    example_strings = generate_examples(ar_text, llt, args.nwords, sub_event, sub_nonsense, prepend_event, random_sampled_words, args.prop_before)
+
+                    # check if this event was annotated from the gold standard
+                    if llt_id in llts_annotated:
+                        string_class = 'is_event'
+                        num_pos += len(example_strings)
+                    else:
+                        string_class = 'not_event'
+                        num_neg += len(example_strings)
+
+                    # NOTE: was using the matching string to match between the two,
+                    # NOTE: but really should be using the LLT MedDRA identifier (above)
+                    # NOTE: keeping this in here so we know what we did initially - NPT 4/19/22
+
+                    # if llt in string_annotated:
+                    #     string_class = 'is_event'
+                    #     num_pos += 1
+                    # else:
+                    #     string_class = 'not_event'
+                    #     num_neg += 1
+
+                    for example_string in example_strings:
+                        writer.writerow([section, drug, llt_id, llt, string_class, example_string])
 
 
-        print(f"\tNumber of terms mentioned in text: {len(llts_mentioned)}")
+            print(f"\tNumber of terms mentioned in text: {len(llts_mentioned)}")
 
-        print(f"\tNumber of positive events: {len(string_annotated & string_mentioned)}")
-        print(f"\tNumber of negative events: {len(string_mentioned - string_annotated)}")
+            print(f"\tNumber of positive events: {len(string_annotated & string_mentioned)}")
+            print(f"\tNumber of negative events: {len(string_mentioned - string_annotated)}")
 
-        print(f"\tNumber of positive training examples: {num_pos}")
-        print(f"\tNumber of negative training examples: {num_neg}")
+            print(f"\tNumber of positive training examples: {num_pos}")
+            print(f"\tNumber of negative training examples: {num_neg}")
 
-        # print(string_annotated - string_mentioned)
-        # print(llts_annotated)
+            # print(string_annotated - string_mentioned)
+            # print(llts_annotated)
 
-        total_num_pos += num_pos
-        total_num_neg += num_neg
+            total_num_pos += num_pos
+            total_num_neg += num_neg
 
     print(f"Finished generating reference set")
     print(f" Total Pos: {total_num_pos}")
