34b0dd92ec37c5b3d2663e34c69fa6d84b7622de
Nicholas Tatonetti <nick.tatonetti@gmail.com>
2022-04-28 23:08:11 -0400

    brining predict uptodate with latest version of the rest of the code

--- src/predict.py
+++ src/predict.py
@@ -4,12 +4,12 @@ predict.py
 # Files to predict on are very large. Will have to split the file first so
 # that it doesn't take forever/use up all the memory on the system.
 # NOTE: this split command cats the header row to each split file:
-tail -n +2 output-part4_app0_clinical_bert_application_set.txt| split -d -C 100m - --filter='sh -c "{ head -n1 output-part4_app0_clinical_bert_application_set.txt; cat; } > $FILE"' output-part4_app0_clinical_bert_application_set_split
+tail -n +2 output-part2_method8_nwords125_clinical_bert_application_set_AR.txt | split -d -C 100m - --filter='sh -c "{ head -n1 output-part2_method8_nwords125_clinical_bert_application_set_AR.txt; cat; } > $FILE"' output-part2_method8_nwords125_clinical_bert_application_set_AR_split
 
 # then you can run with:
 for f in data/*_split*
 do
-    CUDA_VISIBLE_DEVICES=3 python3 src/predict.py --model models/final-bydrug_0_222_24_25_1e-06.pth --examples $f
+    CUDA_VISIBLE_DEVICES=3 python3 src/predict.py --model models/bestepoch-bydrug-CB_8-AR-125_222_24_10_1e-06_256_32.pth --examples $f
 done
 """
 
@@ -20,6 +20,9 @@ import random
 import argparse
 import pandas as pd
 import numpy as np
+
+sys.path.append(os.path.abspath("./src"))
+import fit_clinicalbert as cb
 
 if __name__ == '__main__':
 
@@ -33,28 +36,38 @@ if __name__ == '__main__':
     model_file = os.path.split(model_filepath)[-1]
 
     print(f"Loading model from {model_file}")
+    model_file_noext = os.path.split(model_file)[-1]
 
-    if len(model_file.split('_')) != 6:
-        raise Exception("Model filename not in format expected: {prefix}_{refset}_{np_random_seed}_{random_state}_{EPOCHS}_{LR}.pth")
+    if len(model_file_noext.split('_')) != 8:
+        raise Exception("Model filename not in format expected: {prefix}_{refset}_{np_random_seed}_{random_state}_{EPOCHS}_{LR}_{max_length}_{batch_size}.pth")
 
-    refset = int(model_file.split('_')[1])
-    np_random_seed = int(model_file.split('_')[2])
-    random_state = int(model_file.split('_')[3])
-    EPOCHS = int(model_file.split('_')[4])
-    LR = model_file.split('_')[5].split('.')[0]
+    refset, refsection, refnwords = model_file_noext.split('_')[1].split('-')
+    np_random_seed = int(model_file_noext.split('_')[2])
+    random_state = int(model_file_noext.split('_')[3])
+    EPOCHS = int(model_file_noext.split('_')[4])
+    LR = model_file_noext.split('_')[5]
+    max_length = model_file_noext.split('_')[6]
+    batch_size = model_file_noext.split('_')[7]
 
-    prefix = model_file.split('_')[0]
+    prefix = model_file_noext.split('_')[0]
+    network = prefix.split('-')[2]
+
     print(f"Model")
     print(f"-------------------")
     print(f" prefix: {prefix}")
+    print(f" network: {network}")
     print(f" refset: {refset}")
+    print(f" refsection: {refsection}")
+    print(f" refnwords: {refnwords}")
     print(f" np_random_seed: {np_random_seed}")
     print(f" random_state: {random_state}")
     print(f" EPOCHS: {EPOCHS}")
     print(f" LR: {LR}\n")
+    print(f" max_length: {max_length}")
+    print(f" batch_size: {batch_size}\n")
 
     ex_filename = os.path.split(args.examples)[-1]
-    ex_refset = int(ex_filename.split('_')[1].strip('app'))
+    ex_refset = int(ex_filename.split('_')[1].strip('method'))
     ex_prefix = ex_filename.split('_')[0]
 
     is_split = False
@@ -63,7 +76,7 @@ if __name__ == '__main__':
         is_split = True
         split_no = '-' + ex_filename.split('split')[1]
 
-    results_path = f'./results/{prefix}-{ex_prefix}{split_no}_app{ex_refset}_ref{refset}_{np_random_seed}_{random_state}_{EPOCHS}_{LR}.csv'
+    results_path = f'./results/{prefix}-{ex_prefix}{split_no}_app{ex_refset}_ref{refset}_{np_random_seed}_{random_state}_{EPOCHS}_{LR}_{max_length}_{batch_size}.csv'
 
     print(f"Examples")
     print(f"-------------------")
@@ -77,11 +90,14 @@ if __name__ == '__main__':
         print(f"  > Results file already exists, will not repeat evaluation. If you want to re-generate the results, delete the file and try again.")
         sys.exit(1)
 
+    if network == 'CB':
+        network_path = './models/Bio_ClinicalBERT/'
+    elif network == 'PMB':
+        network_path = './models/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract/'
+    else:
+        raise Exception(f"ERROR: Unknown network: {network}")
 
-    sys.path.append(os.path.abspath("./src"))
-    import fit_clinicalbert as cb
-
-    model = cb.ClinicalBertClassifier()
+    model = cb.ClinicalBertClassifier(network_path)
     model.load_state_dict(torch.load(model_filepath))
 
     # loading the example data
