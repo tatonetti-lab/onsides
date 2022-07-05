679dc4149c042245b2a2a55b7e3c3dfab7830a0c
Cindy <hc3292@cumc.columbia.edu>
2022-07-05 16:16:47 -0400

    Create generate_xml_TAC.py

---
+++ src/generate_xml_TAC.py
@@ -0,0 +1,89 @@
+# generates xml file for outputs
+# make sure the data folder xml_output exists under ./data/
+# last edited 07/05/2022
+
+import xml.etree.ElementTree as ET
+import pandas as pd
+import csv
+
+import argparse
+
+def load_meddra():
+	# load preferred terms and lower level terms
+	meddra_fn = './data/meddra_llt_pt_map.txt'
+	meddra_fh = open(meddra_fn)
+	reader = csv.reader(meddra_fh, delimiter='|')
+	header = next(reader)
+
+	llts = dict()
+
+	for row in reader:
+		data = dict(zip(header, row))
+		llts[data['llt_concept_code']] = data['llt_concept_name'].lower()
+
+	meddra_fh.close()
+
+	return llts
+
+def generate_subelement(row): # row = row index
+	id_reaction = section + str(row[0] + 1) # the id's dont get eval'd so it doesn't matter that much
+	id_norm = id_reaction + str(".N") + str(row[0] + 1)
+	meddra_pt_id = str(row.llt_id)
+	meddra_pt = meddra_dict.get(str(row.llt_id))
+	
+	
+	Reaction = ET.SubElement(Reactions, "Reaction")
+	Reaction.set("id", id_reaction) 
+	Reaction.set("str", meddra_pt)
+	
+	subelement_generated = ET.SubElement(Reaction, "Normalization", id=id_norm, meddra_pt=meddra_pt, meddra_pt_id=meddra_pt_id)
+   
+	return subelement_generated
+
+
+
+if __name__ == "__main__": 
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--results', help="full path to the model results", type=str, required=True)
+	parser.add_argument('--threshold', help="threshold for positive result", type=float, default=2)
+	parser.add_argument('--section', help="part of label (AR, BW, etc)", type=str, default='AR')
+	parser.add_argument('--base-dir', type=str, default='.')
+
+	args = parser.parse_args()
+
+	print(f"Loading data...")
+
+	meddra_dict = load_meddra()
+
+	# PARAMETERS 
+	all_df = pd.read_csv(args.results) #pd.read_csv("grouped-mean-bestepoch-bydrug-CB_0-BW-125_222_24_25_1e-06_256_32.csv")
+	threshold = args.threshold   # this will need to be changed, as well as the pred0 (should be pred1 i think) -- can change this into a param? 
+	section = args.section
+
+	# get vector of different drug names
+	diff_drug_names = pd.unique(all_df.drug)
+
+	for drug_name in diff_drug_names: 
+		
+		# start subsetting data
+		current_drug = str(drug_name)
+		df = all_df[all_df.drug == current_drug]
+
+		# subset data by the threshold
+		df = df[(df['Pred1'] > threshold)]
+		
+		# start to generate xml
+		Label = ET.Element("Label")
+		Label.set("drug", current_drug)
+		Reactions = ET.SubElement(Label, "Reactions")
+
+		# this is the part that generates the xml
+		for row_idx in range(0, len(df)):
+			generate_subelement(df.iloc[row_idx]) 
+
+		tree = ET.ElementTree(Label)
+		filename = args.base_dir + "/data/xml_output/" + current_drug + ".xml"
+		tree.write(filename, xml_declaration=True, encoding='utf-8')
+
+
