b43c400017cf8aa643e84b926c9e96c0ecf20652
zietzm <michael.zietz@gmail.com>
2024-12-16 10:28:36 +0100

    feat(intl/eu): simplify tabular data mapping

--- onsides_intl/onsides_eu/src/onsides_eu/tabular_data_mapping.py
+++ onsides_intl/onsides_eu/src/onsides_eu/tabular_data_mapping.py
@@ -1,87 +1,154 @@
-import numpy as np 
-import pandas as pd
-import requests
-from tqdm import tqdm
-from glob import glob
-import ast, re
-from time import sleep
+"""Find all ADEs listed in the ADEs table.
+
+There are basically 2 steps to this script:
+    1. read each table file to see if it could be an ADE table, discard if not
+    2. read each ADE table and export all words that match a MedDRA term
+"""
+
 import argparse
-import warnings
-warnings.filterwarnings('ignore')
-import os
+import logging
+import pathlib
+
+import pandas as pd
+import polars as pl
+import tqdm.auto as tqdm
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+freqs = ["very common", "common", "uncommon", "rare", "very rare", "not known"]
+socs = [
+    "blood and lymphatic system disorders",
+    "cardiac disorders",
+    "congenital, familial and genetic disorders",
+    "ear and labyrinth disorders",
+    "endocrine disorders",
+    "eye disorders",
+    "gastrointestinal disorders",
+    "general disorders and administration site conditions",
+    "hepatobiliary disorders",
+    "immune system disorders",
+    "infections and infestations",
+    "injury, poisoning and procedural complications",
+    "investigations",
+    "metabolism and nutrition disorders",
+    "musculoskeletal and connective tissue disorders",
+    "neoplasms benign, malignant and unspecified (incl cysts and polyps)",
+    "nervous system disorders",
+    "pregnancy, puerperium and perinatal conditions",
+    "psychiatric disorders",
+    "renal and urinary disorders",
+    "reproductive system and breast disorders",
+    "respiratory, thoracic and mediastinal disorders",
+    "skin and subcutaneous tissue disorders",
+    "social circumstances",
+    "surgical and medical procedures",
+    "vascular disorders",
+    "product issues",
+]
+titles = ["system organ class", "frequency", "adverse events"]
+
+
+def map_tabular(data_folder: pathlib.Path, external_data_folder: pathlib.Path):
+    # Load (drug, ade_text) pairs
+    output_path = data_folder / "raw_ade_table.csv"
+    if output_path.exists():
+        logger.info(f"File already exists, skipping: {output_path}")
+        ade_rows = pl.read_csv(output_path).to_dicts()
+    else:
+        logger.info(f"Extracting raw ADE table from tabular files: {output_path}")
+        ade_rows = extract_raw_ade_table(data_folder)
+
+    # Load MedDRA terms for reference
+    meddra_df = pl.read_csv(external_data_folder / "umls_meddra_en.csv").with_columns(
+        pl.col("STR").str.to_lowercase(),
+        pl.col("STR").str.len_chars().alias("len"),
+    )
+    meddra_name_to_code = meddra_df.to_pandas().set_index("STR")["SDUI"].to_dict()
+    meddra_names = meddra_df.filter(pl.col("TTY").eq("PT") | pl.col("len").gt(5))[
+        "STR"
+    ].to_list()
+
+    logger.info("Updating ADE table with MedDRA terms")
+    updated_rows = list()
+    for row in tqdm.tqdm(ade_rows):
+        ade_text = row["txt"]
+        # remove all of the freqs, socs, and titles
+        for f in freqs:
+            ade_text = ade_text.replace(f, "")
+        for s in socs:
+            ade_text = ade_text.replace(s, "")
+        for t in titles:
+            ade_text = ade_text.replace(t, "")
+
+        for meddra_name in meddra_names:
+            if meddra_name in ade_text:
+                code = meddra_name_to_code[meddra_name]
+                new_row = {
+                    "drug": row["drug"],
+                    "exact_match_list": meddra_name,
+                    "matched_codes": code,
+                }
+                updated_rows.append(new_row)
+
+    pl.DataFrame(updated_rows).write_csv(data_folder / "parsed_ade_tabular.csv")
+
+
+def extract_raw_ade_table(data_folder: pathlib.Path) -> list[dict[str, str]]:
+    tbl_files = list(data_folder.joinpath("raw_tbl").glob("*.csv"))
+
+    ade_table_words = [
+        "system",
+        "very common",
+        "common",
+        "not known",
+        "rare",
+        "uncommon",
+    ]
+    ade_table_rows = list()
+    for tbl_file in tqdm.tqdm(tbl_files):
+        df = pd.read_csv(tbl_file)
+
+        # Check if the dataframe contains any of the words that indicate it
+        # might be an ADE table.
+        first_line = " ".join(df.columns.tolist())
+        word_match = [word for word in ade_table_words if word in first_line]
+        if not any(word_match):
+            continue
+
+        # Convert the dataframe to a string in column-major order (i.e. concat
+        # columns then concat rows), dropping any nan values along the way.
+        table_values = df.values.flatten("F").tolist()
+        table_string = " ".join(v for v in table_values if isinstance(v, str))
+        table_string = table_string.replace("- ", "").replace("\r", " ").lower()
+
+        drug_name = tbl_file.stem
+        row = {"drug": drug_name, "txt": table_string}
+        ade_table_rows.append(row)
+
+    pl.DataFrame(ade_table_rows).write_csv(data_folder / "raw_ade_table.csv")
+    return ade_table_rows
+
 
 def main():
-    parser = argparse.ArgumentParser(description='let the code know where the data is held')
-    parser.add_argument('--data_folder', required=True, help='Path to the data folder.')
-    parser.add_argument('--external_data', required=True, help='Path to the where the external data is housed.')
-
+    parser = argparse.ArgumentParser(
+        description="let the code know where the data is held"
+    )
+    parser.add_argument(
+        "--data_folder",
+        type=pathlib.Path,
+        required=True,
+        help="Path to the data folder.",
+    )
+    parser.add_argument(
+        "--external_data",
+        type=pathlib.Path,
+        required=True,
+        help="Path to the where the external data is housed.",
+    )
     args = parser.parse_args()
-    data_folder = args.data_folder
-    external_data_folder = args.external_data
+    map_tabular(args.data_folder, args.external_data)
 
-    #get list of all tabular files.
-    tbls = glob(data_folder+'raw_tbl/*')
 
-    #iterate through all tabular files and extract the relevant tables (i.e. those with the columns of interest)
-    ade_tbl = []
-    for tbl in tqdm(tbls):
-        drug = tbl.split('/')[-1].split('_')[0]
-        try:
-            df = pd.read_csv(tbl)
-            if any([i in list(df.columns) for i in ['System', 'Very common', 'Common', 'Not known', 'Rare', 'Uncommon']]):
-                col = df.columns.tolist()
-                for i in col:
-                    l = ' '.join([i for i in df[i].tolist() if str(i) != 'nan'])
-                    ade_tbl.append([drug, i, l])
-        except:
-            pass
-    
-    ades_df = pd.DataFrame(ade_tbl, columns = ['drug', 'col', 'txt'])
-    ades_df.to_csv(data_folder+'raw_ade_table.csv', index=False)
-
-    ##############################################################################################################
-    ades_df = pd.read_csv(data_folder+'data/raw_ade_table.csv')
-    ades_df['ade'] = ades_df['txt'].apply(lambda x: str(x).replace('- ', ''))
-
-    freqs = ['very common', 'common', 'uncommon', 'rare', 'very rare', 'not known']
-    socs =  ['blood and lymphatic system disorders','cardiac disorders', 'congenital, familial and genetic disorders',
-            'ear and labyrinth disorders', 'endocrine disorders', 'eye disorders', 'gastrointestinal disorders', 
-            'general disorders and administration site conditions', 'hepatobiliary disorders', 'immune system disorders',
-            'infections and infestations', 'injury, poisoning and procedural complications', 'investigations', 'metabolism and nutrition disorders',
-            'musculoskeletal and connective tissue disorders', 'neoplasms benign, malignant and unspecified (incl cysts and polyps)',
-            'nervous system disorders', 'pregnancy, puerperium and perinatal conditions', 'psychiatric disorders',
-            'renal and urinary disorders', 'reproductive system and breast disorders', 'respiratory, thoracic and mediastinal disorders',
-            'skin and subcutaneous tissue disorders', 'social circumstances', 'surgical and medical procedures', 'vascular disorders', 'product issues']
-    titles = ['system organ class', 'frequency', 'adverse events']
-
-    meddra_df = pd.read_csv(external_data_folder+'umls_meddra_en.csv')
-    meddra_df['STR'] = meddra_df.STR.apply(lambda x: x.lower())
-    meddra_df['len'] = meddra_df.STR.apply(lambda x: len(x))
-    meddra_dict = dict(zip(meddra_df.STR, meddra_df.SDUI))
-    meddra_df = meddra_df[(meddra_df.TTY == 'PT')|(meddra_df['len'] > 5)]
-
-    found_ades = []
-    meddra_names = meddra_df.STR.tolist()
-    for ade_text in tqdm(df.ade.tolist()):
-        ar_text = ' '.join(ade_text.split()).lower() 
-        #remove all of the freqs, socs, and titles
-        for f in freqs:
-            ar_text = ar_text.replace(f, '')
-        for s in socs:
-            ar_text = ar_text.replace(s, '')
-        for t in titles:
-            ar_text = ar_text.replace(t, '')
-        found_terms = []
-        for concept_name in meddra_names:
-            if ar_text.find(concept_name) == -1:
-                continue
-            else:
-                found_terms.append(concept_name)
-        found_ades.append(found_terms)
-    
-    ades_df['exact_match_list'] = found_ades
-    ades_df['matched_codes'] = ades_df.exact_match_list.apply(lambda x: [meddra_dict[i] for i in x] if str(x) != 'nan' else None)
-    ades_df.to_csv(data_folder+'parsed_ade_tabular.csv', index=False)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
