792ede3e776e3f7a8fe0298fe2f2c8cb20a0b7e7
Michael Zietz <michael.zietz@gmail.com>
2024-12-17 05:39:33 -0600

    feat(intl/eu): fix text data parsing and section extraction (#65)
    * chore(intl/eu): drop unused file
    * feat(intl/eu): clean up the text data parsing file
    Exactly same order of operations as before
    * feat(intl/eu): unify text_data_parse, remove unnecessary steps

--- onsides_intl/onsides_eu/src/onsides_eu/text_data_parse.py
+++ onsides_intl/onsides_eu/src/onsides_eu/text_data_parse.py
@@ -1,124 +1,93 @@
-import numpy as np 
-import pandas as pd
-import requests
-from tqdm import tqdm
-from glob import glob
-import ast, re, json, orjson
-from time import sleep
 import argparse
-import warnings
-warnings.filterwarnings('ignore')
-import os
+import logging
+import pathlib
+import re
+
+import polars as pl
+import tqdm.auto as tqdm
+from pydantic import BaseModel
+
+logger = logging.getLogger(__name__)
+
+
+class LabelSections(BaseModel):
+    drug: str
+    clinical_particulars: str
+    undesirable_effects: str
+    ade_text: str
+
+
+def extract_undesirable_effects(
+    raw_txt_file: pathlib.Path, output_dir: pathlib.Path
+) -> LabelSections | None:
+    with open(raw_txt_file) as f:
+        txt = f.read()
+    normed = " ".join(txt.split())
+
+    regex = (
+        r"(4\. CLINICAL|4 CLINICAL|4\. CLINCAL|4\. Clinical).+"
+        r"(5\. PHARMACOLOGIC|5 PHARMACOLOGIC)"
+    )
+    match = re.search(regex, normed)
+    if match is None:
+        logger.error(f"Could not find 4. CLINICAL in {raw_txt_file}")
+        return None
+    clinical_particulars = match.group()
+
+    regex = r"(4\.8\. Undesirable|4\.8 Undesirable).+(4\.9 Overdose|4\.9\. Overdose)"
+    match = re.search(regex, clinical_particulars)
+    if match is None:
+        logger.error(f"Could not find 4.8 Undesirable in {raw_txt_file}")
+        return None
+    undesirable_effects = match.group()
+    clean_ade = re.sub(r"[^\n\w\s.,]", "", undesirable_effects)
+
+    drug_name = raw_txt_file.stem
+    sections = LabelSections(
+        drug=drug_name,
+        clinical_particulars=clinical_particulars,
+        undesirable_effects=undesirable_effects,
+        ade_text=clean_ade,
+    )
+    output_path = output_dir.joinpath(drug_name).with_suffix(".json")
+    with open(output_path, "w") as out:
+        json_str = sections.model_dump_json(indent=2)
+        out.write(json_str)
+    return sections
+
+
+def parse_all_text(data_folder: pathlib.Path) -> None:
+    txt_files = list(data_folder.joinpath("raw_txt").glob("*"))
+    json_folder = data_folder.joinpath("json")
+    json_folder.mkdir(exist_ok=True)
+
+    sections_table = list()
+    n_failed = 0
+    for txt_file in tqdm.tqdm(txt_files):
+        maybe_sections = extract_undesirable_effects(txt_file, json_folder)
+        if maybe_sections is not None:
+            sections_table.append(maybe_sections)
+        else:
+            n_failed += 1
+
+    logger.info(f"Extracted data for {len(sections_table)}, {n_failed} failures")
+
+    output_file = data_folder / "ade_text_table.csv"
+    pl.DataFrame(sections_table).select("drug", "ade_text").write_csv(output_file)
+    logger.info("finish extracting text data")
+
 
 def main():
-    parser = argparse.ArgumentParser(description='let the code know where the data is held')
-    parser.add_argument('--data_folder', required=True, help='Path to the data folder.')
-    parser.add_argument('--external_data', required=True, help='Path to the where the external data is housed.')
-
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--data_folder",
+        type=pathlib.Path,
+        required=True,
+        help="Path to the data folder.",
+    )
     args = parser.parse_args()
-    data_folder = args.data_folder
-    external_data_folder = args.external_data
+    parse_all_text(args.data_folder)
 
-    #get list of all tabular files.
-    txts = glob(data_folder+'raw_txt/*')
 
-    isExist = os.path.exists(data_folder+'raw_json')
-    if not isExist:
-        os.mkdir(data_folder+'raw_json')
-
-    #add in titles as reference. may use these in the future.
-    titles = ['1. NAME OF THE MEDICINAL PRODUCT','2. QUALITATIVE AND QUANTITATIVE COMPOSITION', '3. PHARMACEUTICAL FORM', '4. CLINICAL PARTICULARS', 
-              '5. PHARMACOLOGICAL PROPERTIES', '6. PHARMACEUTICAL PARTICULARS', '7. MARKETING AUTHORISATION HOLDER', '8. MARKETING AUTHORISATION NUMBER',
-              '9. DATE OF FIRST AUTHORISATION/RENEWAL OF THE AUTHORISATION','10. DATE OF REVISION OF THE TEXT']
-
-    #iterate through all text files and extract the 4. CLINICAL PARTICULARS section
-    not_found = []
-    not_found_next = []
-    for txt in tqdm(txts):
-        drug = txt.split('/')[-1].replace('.txt','')
-        with open(txt, 'r+') as fi:
-            txt = fi.read()
-            txt = txt.split('ANNEX II')[0]
-        #we're interested in "4.CLINICAL PARTICULARS". extract this section
-        if '4. CLINICAL' in txt:
-            if '5. PHARMACOLOGICAL' in txt:
-                sec_dict = {}
-                sec_dict['4. CLINICAL PARTICULARS'] = '4. CLINICAL'+txt.split('4. CLINICAL')[1].split('5. PHARMACOLOGICAL')[0]+'5. PHARMACOLOGICAL'
-                with open(data_folder+'raw_json/{}.json'.format(drug), "w") as out:
-                    json.dump(sec_dict, out)
-            elif '6. PHARMACEUTICAL' in txt:
-                sec_dict = {}
-                sec_dict['4. CLINICAL PARTICULARS'] = '4. CLINICAL'+txt.split('4. CLINICAL')[1].split('6. PHARMACEUTICAL')[0]+'6. PHARMACEUTICAL'
-                with open(data_folder+'raw_json/{}.json'.format(drug), "w") as out:
-                    json.dump(sec_dict, out)
-            else:
-                not_found_next.append(txt)
-        else:
-            if '4.1 Therapeutic' in txt:
-                sec_dict = {}
-                sec_dict['4. CLINICAL PARTICULARS'] = '4. CLINICAL PARTICULARS'+txt.split('4.1 Therapeutic')[1].split('5. PHARMACOLOGICAL')[0]+'5. PHARMACOLOGICAL'
-                with open(data_folder+'raw_json/{}.json'.format(drug), "w") as out:
-                    json.dump(sec_dict, out)
-            elif '4. CLINICAL' in txt.replace('\n',''):
-                sec_dict = {}
-                sec_dict['4. CLINICAL PARTICULARS'] = '4. CLINICAL'+txt.replace('\n','').split('4. CLINICAL')[1].split('5. PHARMACOLOGICAL')[0]+'5. PHARMACOLOGICAL'
-                with open(data_folder+'raw_json/{}.json'.format(drug), "w") as out:
-                    json.dump(sec_dict, out)
-            elif '4. CLINICAL' in ' '.join(txt.split()):
-                sec_dict = {}
-                sec_dict['4. CLINICAL PARTICULARS'] = '4. CLINICAL'+' '.join(txt.split()).split('4. CLINICAL')[1].split('5. PHARMACOLOGICAL')[0]+'5. PHARMACOLOGICAL'
-                with open(data_folder+'raw_json/{}.json'.format(drug), "w") as out:
-                    json.dump(sec_dict, out)
-            else:
-                not_found.append(txt)
-        len(not_found), len(not_found_next)
-
-    ########################################################
-    #now, we want to extract the ADEs from the 4. CLINICAL PARTICULARS section
-    isExist = os.path.exists(data_folder+'raw_json_ue')
-    if not isExist:
-        os.mkdir(data_folder+'raw_json_ue')
-
-    sec4_titles = ['4.1 Therapeutic indications', '4.2 Posology and method of administration', '4.3 Contraindications', '4.4 Special warnings and precautions for use', 
-                   '4.5 Interaction with other medicinal products and other forms of interaction', '4.6 Fertility, pregnancy and lactation', '4.7 Effects on ability to drive and use machines',
-                   '4.8 Undesirable effects', '4.9 Overdose']
-
-    jsons = glob(data_folder+'raw_json/*')
-    not_found_ue = []
-    for j in tqdm(jsons):
-        with open(j, 'r+') as fi:
-            d = orjson.loads(fi.read())
-        try:
-            txt = d['4. CLINICAL PARTICULARS']
-            txt = ' '.join(txt.split())
-            ue = txt.split('4.8 Undesirable')[1].split('4.9 Overdose')[0]
-            ue_dict = {}
-            ue_dict['Undesirable effects'] = ue
-            drug = j.split('/')[-1].replace('.json','')
-            with open(data_folder+'raw_json_ue/{}.json'.format(drug), "w") as out:
-                json.dump(ue_dict, out)
-        except:
-            not_found_ue.append(json)
-
-    jsons = glob(data_folder+'raw_json_ue/*')
-    print('we found the undesirable effects section for {j} and weren\'t able to find it for {nf}'.format(j = str(len(jsons)), nf = str(len(not_found_ue))))
-
-    ########################################################
-    #now, we format the json files into a table to use. 
-    ade_list = []
-    for j in tqdm(jsons):
-        with open(j, 'r+') as fi:
-            d = orjson.loads(fi.read())
-        ade = d['Undesirable effects']
-        pattern = r'[^\n\w\s.,]'
-        clean_ade = re.sub(r'[^\n\w\s.,]', '', ade)
-        drug = j.split('/')[-1].replace('.json','')
-        ade_list.append([drug, clean_ade])
-    
-    ade_text_table_df = pd.DataFrame(ade_list, columns=['drug', 'ade_text'])
-    ade_text_table_df.to_csv(data_folder+'ade_text_table.csv', index=False)
-
-    print('finish extracting text data.')
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
