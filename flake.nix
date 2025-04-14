ca17ca935395ecf7d7b961369005d8aaa8df8453
Michael Zietz <michael.zietz@gmail.com>
2025-04-14 16:36:28 -0700

    feat: update nix development environment

--- flake.nix
+++ flake.nix
@@ -1,66 +1,58 @@
 {
-  description = "Python environment with tabula-py and uv package manager using Fish shell";
+  description = "Dev env for OnSIDES, including tabula, uv, pandoc, snakemake, etc.";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
       in
       {
         devShells.default = pkgs.mkShell {
-          # Provide fish in the environment
           buildInputs = [
-            pkgs.fish
             pkgs.uv
             pkgs.jdk11
             pkgs.pandoc
             pkgs.duckdb
           ];
 
-          # Even though we say "shell = fish", the shellHook is still parsed by Bash!
-          # So we keep our shellHook in Bash syntax, then manually exec fish:
-          shell = "${pkgs.fish}/bin/fish";
-
           shellHook = ''
-            # -- Bash syntax for environment exports:
             export JAVA_HOME=${pkgs.jdk11}/lib/openjdk
             export TABULA_JAVA_PATH=${pkgs.jdk11}/lib/openjdk/bin/java
-
+            echo
             if [ ! -d ".venv" ]; then
               echo "Creating new virtual environment with uv..."
               ${pkgs.uv}/bin/uv sync
             fi
 
-            # At this point, we forcibly replace Bash with Fish. The `-C` argument
-            # lets us specify some Fish code to run immediately upon starting.
-            #
-            # Within that Fish code, we can safely use 'source .venv/bin/activate.fish'
-            # to activate the venv the proper Fish way, and then echo any helpful tips.
+            source .venv/bin/activate
 
-            exec ${pkgs.fish}/bin/fish -C '
-              set fish_greeting
-              function fish_prompt
-                set_color green
-                echo -n (prompt_pwd)
-                set_color normal
-                echo -n "> "
-              end
+            test_tabula() {
+              python -c "import tabula" >/dev/null 2>&1
+              if [ $? -eq 0 ]; then
+                echo "Onsides development environment activated!"
+              else
+                echo "There was an error installing tabula. Please check your installation."
+              fi
+            }
+            test_tabula
 
-              source .venv/bin/activate.fish
-
-              echo "Onsides development environment activated!"
-              echo "To install additional packages:"
-              echo "  uv add <package-name>"
-              echo ""
-              echo "To verify tabula-py installation:"
-              echo "  python -c \"import tabula; print(tabula.__version__)\""
-            '
+            echo "To install additional Python packages:"
+            echo "  uv add <package-name>"
+            echo "To install additional non-Python packages, modify flake.nix."
+            echo ""
           '';
         };
-      });
+      }
+    );
 }
